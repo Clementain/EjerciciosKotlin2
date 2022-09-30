bfcaa87d9831090f5fb266f64ad591e75fbd15cc
Clementain <clentecarmona@gmail.com>
2022-09-29 20:06:17 -0500

    Se cmbiaron los valores del mapa de las calificaciones de Double a Int

--- src/main/kotlin/Ejercicio4.kt
+++ src/main/kotlin/Ejercicio4.kt
@@ -1,20 +1,20 @@
 fun main() {
-    val kardex: Map<String, Double> = mapOf(
-        "matematicas" to 50.0, "geografia" to 73.0, "español" to 80.0, "quimica" to 93.0, "historia" to 99.0
+    val kardex: Map<String, Int> = mapOf(
+        "matematicas" to 50, "geografia" to 73, "español" to 80, "quimica" to 93, "historia" to 99
     )
     println(calif(kardex))
 
 }
 
-fun calif(notas: Map<String, Double>): Map<String, String> {
+fun calif(notas: Map<String, Int>): Map<String, String> {
     val des: MutableMap<String, String> = mutableMapOf()
     for ((k, v) in notas) {
         val d = when (v) {
-            in 0.0..69.0 -> "Desempeño insuficiente"
-            in 70.0..74.0 -> "Suficiente"
-            in 75.0..84.0 -> "Bueno"
-            in 85.0..94.0 -> "Notable"
-            in 95.0..100.0 -> "Excelente"
+            in 0..69 -> "Desempeño insuficiente"
+            in 70..74 -> "Suficiente"
+            in 75..84 -> "Bueno"
+            in 85..94 -> "Notable"
+            in 95..100 -> "Excelente"
             else -> " "
         }
         des[k.uppercase()] = d
